737d18fbc4d198ae1587801ca2f59ff09fa134c6
sam <sam@email>
2024-01-11 19:26:31 +0200

    removed testing data

--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./build/mmplat --bind localhost:8080 --log debug -r --folders /home/sam/Documents/projects/golang/mmplat/samples,/home/sam/Documents/projects/golang/mmplat/t --login-data anton 
